259f3294f788e20e481295e5fd403c4ca3a73769
sidhirpara <jaydphirpara@gmail.com>
2025-03-25 01:18:17 +0530

    modiefied main.tsx for QR

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+if (window.location.pathname === "/qr") {
+  window.location.replace("/#/qr");
+}
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
